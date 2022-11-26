264bc6e071b366cf35ba53ccbfd77fe068a442eb
José Gutiérrez <pepenauta.developer@gmail.com>
2022-11-25 18:42:47 -0600

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { LoginComponent } from './auth/login/login.component';
 import { PagesComponent } from './pages/pages.component';
 
@@ -37,7 +37,6 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [],
   imports: [
     RouterModule.forRoot(routes)
   ],
