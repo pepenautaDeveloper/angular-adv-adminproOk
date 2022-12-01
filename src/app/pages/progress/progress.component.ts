b431aa0ec9fd4f233a72ab0a444fc0f519fdb10e
José Gutiérrez <pepenauta.developer@gmail.com>
2022-11-30 19:07:10 -0600

    Cambios

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
+  progreso: number = 50;
+
+  get getPorcentaje() {
+    return `${ this.progreso }%`;   
+  }
+
+  cambiarValor ( valor: number ) {
+
+    this.progreso = this.progreso + valor;
+    
+  }
+
 }
