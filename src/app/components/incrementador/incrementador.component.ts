5f3807f3d9edd51a6896f6ac3c0f3fa7ddce5bb9
pepenautamx <112117801+pepenautaDeveloper@users.noreply.github.com>
2022-12-27 00:15:22 -0600

    output

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -7,9 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class IncrementadorComponent {
 
- 
-
  @Input('valor') progreso: number = 40;
+ 
+ @Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
 
 
@@ -19,15 +19,22 @@ export class IncrementadorComponent {
 
   cambiarValor ( valor: number ) {
 
-    if ( this.progreso + valor > 100 ) {
-      return;
-      }
-      if ( this.progreso + valor < 0 ) {
-      return;
-      }
-      this.progreso = this.progreso + valor;
-      }
+    if ( this.progreso >= 100 && valor >= 0 ) {
+     this.valorSalida.emit(100); 
+     return this.progreso = 100;
+    }
+    
+    if ( this.progreso <= 0 && valor < 0 ) {
+   this.valorSalida.emit(0); 
+     return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
 
       
+
+}
+
 
 }
